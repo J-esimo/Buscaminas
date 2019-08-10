78378b193357062c10b314eb64e00f6c4dd165f6
Eratorn <isaigm23@gmail.com>
2019-08-09 19:34:13 -0500

    first commit

---
+++ Buscaminas.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 09:53:38 2019
+@author: isai
+"""
+import sys, random as rand, pygame as py
+from pygame.locals import *
+
+py.init()
+py.mixer.quit()
+
+class Block(py.sprite.Sprite):
+    def __init__(self, width, height, x, y):
+        py.sprite.Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = py.Surface([width, height])
+        self.color = (127, 127, 127)
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.nBombs = 0
+        self.is_bomb = False
+        self.is_visible = False
+        self.font = py.font.SysFont("Chandas", 15)
+        self.text = None
+        self.text_rect = None
+        py.draw.rect(self.image, self.color, [0, 0, width, height])
+
+class Game:
+    def __init__(self):
+        self.width = 1280
+        self.height = 720
+        self.isRunning = True
+        self.window = py.display.set_mode([self.width, self.height])
+        self.sprites = py.sprite.Group()
+        self.rows = 10
+        self.cols = 10
+        self.x, self.y = 500, 200
+        self.w, self.h = 30, 30
+        self.matrix = []
+        self.bombs = 13
+        py.display.set_caption("Buscaminas")
+        for i in range(self.rows):
+            self.matrix.append([])
+            for j in range(self.cols):
+                self.matrix[i].append(Block(self.w, self.h, self.x + j * (self.w + 5), self.y + i * (self.h + 5)))
+        for i in range(self.bombs):
+            x = rand.randint(0, self.rows - 1)
+            y = rand.randint(0, self.cols - 1)
+            self.matrix[x][y].is_bomb = True
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if not self.matrix[i][j].is_bomb:
+                    self.matrix[i][j].nBombs = self.countBombs(i, j)           
+        self.sprites.add(self.matrix)
+        
+    def gameLoop(self):
+        clock = py.time.Clock()
+        while(self.isRunning):
+            self.events()
+            self.draw()
+            clock.tick(60)
+    
+    def events(self):
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                self.isRunning = False
+                py.quit()
+                sys.exit(0)
+            if event.type == py.MOUSEBUTTONDOWN and event.button == 1:
+                mouse_position = py.mouse.get_pos()
+                for sprite in self.sprites:
+                    if sprite.rect.collidepoint(mouse_position) and not sprite.is_bomb:
+                        sprite.is_visible = True    
+                        sprite.text = sprite.font.render(str(sprite.nBombs), True, (0, 0, 0))
+                        sprite.text_rect = sprite.text.get_rect(center=sprite.rect.center)
+                    elif sprite.rect.collidepoint(mouse_position) and sprite.is_bomb:
+                        print("Boom!")
+    
+    def countBombs(self, row, col):
+        count = 0
+        for i in range(row - 1, row + 2):
+            for j in range(col - 1, col + 2):
+                if i < 0 or i >= self.rows or j < 0 or j >= self.cols:
+                    continue
+                if i == row and j == col:
+                    continue
+                elif self.matrix[i][j].is_bomb:
+                    count += 1
+        return count
+    
+    def draw(self):
+        self.window.fill((255, 255, 255))
+        self.sprites.draw(self.window)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.matrix[i][j].is_visible:
+                    self.window.blit(self.matrix[i][j].text, self.matrix[i][j].text_rect)
+        py.display.flip()
+
+def main():
+    game = Game()
+    game.gameLoop()
+    
+if __name__ == '__main__':
+    main()
+
+
+
+    
